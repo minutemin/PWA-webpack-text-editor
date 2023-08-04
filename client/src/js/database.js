587dc975fb7c0a34bf210c2b4f65c4a91d410c06
sarahm <minutemin@gmail.com>
2023-08-03 18:15:26 -0700

    add putDb method that enters into Db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,9 +12,16 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.log('Put to database');
+  const jateDB = await openDB ('jate', 1,);
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const results = await request;
+  return results?.value;
+}
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
 
